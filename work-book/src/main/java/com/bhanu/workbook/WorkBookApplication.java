9ee99a0f7957333922288db34a8c29296aabfc02
Bhanuraj KM <b0k02nq@homeoffice.wal-mart.com>
2021-04-08 19:46:48 +0530

    adding work-book

---
+++ work-book/src/main/java/com/bhanu/workbook/WorkBookApplication.java
@@ -0,0 +1,13 @@
+package com.bhanu.workbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkBookApplication.class, args);
+	}
+
+}
